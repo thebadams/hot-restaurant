37267c112cff602ed9a794f2b8ace4afb447edba
Brian Adams <bpeteradams@gmail.com>
2021-04-18 00:01:56 -0400

    reboot

--- server.js
+++ server.js
@@ -1,39 +1,12 @@
-const Express = require("express");
-const Path = require("path")
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 
-const app = express()
-const PORT = 8080
-
-class Reservation{
-    constructor(name, id, phone, email){
-        this.name = name,
-        this.id = id,
-        this.phone = phone,
-        this.email = email,
-        this.routeName = this.name.replace(/\s/g, '').toLowerCase()
-    }
-}
-
-const reservations = []
-
-const waitList = []
-//app.get logic here
-    //get routes: /api/tables; /api/waitlist
-    
-
-//app.post logic here
-//post routes: //api/tables; /api/waitlist
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
-//html display routes:
 
-// root: display home.html
-
-app.get("/", sendFile("./home.html"))
-// "/reserve": display reserve.html
-app.get("/reserve", sendFile("./reserve.html"))
-// "/tables": display tables.html
-app.get("/tables", sendFile("./tables.html"))
-
-module.exports = {Reservation, reservations}
+app.listen(PORT, ()=> console.log(`Server Opened At ${PORT}`))
